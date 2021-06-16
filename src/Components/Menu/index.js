d1a8c98a380b9e83c3af93a2dc3872436ecdb554
monzoor-rm <monzoor.morshed@raremark.com>
2021-06-16 13:57:48 +0600

    add sub menu bg

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -1,31 +1,24 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
-import { PATHS } from '@constants';
+// import { PATHS } from '@constants';
 import { Icon } from '@components';
 
 const Menu = () => {
   return (
-    <div className="container-fluid ">
-      <div className="row">
-        <div className="col-4 bg-primar">
-          <div className="row justify-content-end">
-            <div className="col-auto py-2 pe-5">
-              <Icon className="me-4" icon={['fas', 'bars']} />
-              Browse categories
+    <div className="container-fluid bg-primary-dark">
+      <div className="container">
+        <div className="row">
+          <div className="col-3 bg-primary text-white py-3">
+            <div className="row">
+              <div className="col-auto py-2 pe-5">
+                <Icon className="me-4" icon={['fas', 'bars']} />
+                Browse categories
+              </div>
             </div>
           </div>
+          <div className="col bg-dark py-3">oka</div>
         </div>
-        <div className="col-8 bg-dark">oka</div>
       </div>
-      {/* <Icon icon={faCoffee} />
-      <ul>
-        <li>
-          <Link to={PATHS.HOME}>Home</Link>
-        </li>
-        <li>
-          <Link to={PATHS.PAGE1}>Page 1</Link>
-        </li>
-      </ul> */}
     </div>
   );
 };
