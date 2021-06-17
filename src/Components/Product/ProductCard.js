2dc109ae1186e0fb5ade9688ec4ab90778e8bdc6
monzoor-rm <monzoor.morshed@raremark.com>
2021-06-18 04:05:10 +0600

    Add product list

--- src/Components/Product/ProductCard.js
+++ src/Components/Product/ProductCard.js
@@ -9,6 +9,7 @@ const ProductCard = ({
   topSlider = false,
   midBanner = false,
   recently = false,
+  list = false,
   className,
 }) => {
   const {
@@ -20,12 +21,14 @@ const ProductCard = ({
     sell_price,
     image,
     links,
+    rating,
   } = content;
 
   const rowClasses = classNames('row', {
     'align-items-end bg-gray-300 border border-start-0': topSlider,
     'p-5 align-items-center': midBanner,
     'border p-3 mx-0 align-items-center ': recently,
+    'border border-end-0 mb-3': list,
     [className]: !!className,
   });
 
@@ -33,12 +36,14 @@ const ProductCard = ({
     'col-6 px-5 py-5': topSlider,
     'col-7': midBanner,
     'col-7 order-2': recently,
+    'col-12 order-2': list,
   });
 
   const imageWrapperClasses = classNames('', {
     'col-6 pe-0': topSlider,
     'col-5': midBanner,
     'col-5 order-1': recently,
+    'col-12 order-1 pt-2 mb-3': list,
   });
 
   const wrapperClasses = classNames({ 'mx-4': topSlider });
@@ -46,30 +51,49 @@ const ProductCard = ({
   const titleClasses = classNames('product-title', {
     'fs-1 text-uppercase text-lh-1': topSlider,
     'fs-3 text-lh-1': midBanner,
-    'fs-5 fw-bold': recently,
+    'fs-5 fw-bold mb-0': recently,
+    'mb-2': list,
   });
 
   const descriptionClasses = classNames('text-muted', {
     'text-capitalize': topSlider,
+    'd-none': list,
   });
 
-  const buttonClasses = classNames(
-    'btn btn-primary text-uppercase text-white',
-    {
-      'mb-5 btn-lg': topSlider,
-    },
-  );
+  const priceClasses = classNames('me-2 text-orange', {
+    'fs-2': topSlider,
+    'fs-5 fw-bold': list,
+  });
+
+  const sellPriceClasses = classNames('text-decoration-line-through fw-bold', {
+    small: list,
+  });
+
+  const buttonClasses = classNames('btn btn-primary  text-white', {
+    'mb-5 btn-lg text-uppercase': topSlider,
+    'fs-6 mb-3 px-2': list,
+  });
+
+  const ratingItems = [];
+
+  if (list) {
+    for (let i = 1; i <= 5; i++) {
+      if (i > rating) {
+        ratingItems.push(<i class="far text-small text-orange fa-star"></i>);
+      } else {
+        ratingItems.push(<i class="fas text-small text-orange fa-star"></i>);
+      }
+    }
+  }
 
   return (
     <div className={rowClasses}>
       <div className={contentWrapperClasses}>
         <div className={wrapperClasses}>
-          {category ? (
+          {category && (
             <p className="text-uppercase text-orange text-spacing-5">
               {category}
             </p>
-          ) : (
-            ''
           )}
 
           <p
@@ -78,25 +102,19 @@ const ProductCard = ({
           />
           <p className={descriptionClasses}>{description}</p>
 
-          {price ? (
+          {list ? <span className="d-block mb-2">{ratingItems}</span> : ''}
+          {price && (
             <p>
-              {sell_price ? (
-                <span className="text-decoration-line-through fw-bold">
-                  {sell_price}
-                </span>
-              ) : (
-                ''
+              <span className={priceClasses}>{price}</span>
+              {sell_price && (
+                <span className={sellPriceClasses}>{sell_price}</span>
               )}
-
-              <span className="text-orange fs-2 ms-3">{price}</span>
             </p>
-          ) : (
-            ''
           )}
 
           {!links ? (
             <Link to={`/details/${slug}`} className={buttonClasses}>
-              start buying
+              {list ? 'ADD TO CART ' : 'start buying'}
             </Link>
           ) : (
             <>
@@ -120,7 +138,7 @@ const ProductCard = ({
         </div>
       </div>
       <div className={imageWrapperClasses}>
-        <img className="img-fluid" src={image} alt={description} />
+        <img className="img-fluid test" src={image} alt={description} />
       </div>
     </div>
   );
--- src/Containers/Home/NewArrivals.js
+++ src/Containers/Home/NewArrivals.js
@@ -1,19 +1,26 @@
 import { ProductCard, SectionHeader, SpecialOffer } from '@components';
 import specialOffer from 'data/specialOffer.json';
+import productList from 'data/productList.json';
 
 const NewArrivals = () => {
   return (
     <>
       <SectionHeader title="NEW ARRIVALS" icon="fas fa-trophy me-2" />
-      <div className="row mx-0">
-        <div className="col-5 border border-end-0 p-2">
+      <div className="row mx-0 border-end">
+        <div
+          className="col-5 border border-end-0 p-2"
+          style={{ height: '847px' }}
+        >
           <SpecialOffer content={specialOffer} />
         </div>
         <div className="col-7">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est esse
-          recusandae aspernatur et, at ducimus? Nihil dolor ullam illum
-          perferendis, nesciunt eum pariatur repellat temporibus totam.
-          Voluptatibus quod aut eaque.
+          <div className="row">
+            {productList.map((product) => (
+              <div key={product.id} className="col-4">
+                <ProductCard content={product} list />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
